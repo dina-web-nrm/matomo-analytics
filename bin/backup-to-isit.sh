ca1ea5f991b43367692e07c39eb0d031acc71cfb
Ingimar Erlingsson <inkimar@gmail.com>
2020-08-06 11:28:06 +0200

    adding 'scp' to biobackup

--- bin/backup-to-isit.sh
+++ bin/backup-to-isit.sh
@@ -5,3 +5,5 @@ TARGET=../backups
 
 docker exec test-matomo_db_1 bash -c \
 	'mysqldump -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}' | gzip > ${TARGET}/${MYSQL_DATABASE}-${NOW}.sql.gz
+
+scp ${TARGET}/${MYSQL_DATABASE}-${NOW}.sql.gz biobackup:/backup/matomo &>>biobackup.log
